2767b30dbeedb8e32bbb1e79432ad0478cb1558e
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 00:59:09 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,12 +12,10 @@ class Genre
   end
 
   def songs
-    # find all songs with matching genre
     Song.all.select {|s| s.genre == name}
   end
 
   def artists
     songs.map {|s| s.artist}
-    # collects each artist for every song in the main collect that has a matching genre
   end
 end
