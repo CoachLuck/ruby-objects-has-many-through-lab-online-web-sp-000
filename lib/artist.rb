30ed7ab5024edfa69f5d6db44949016f82156ea6
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 00:55:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,15 +11,18 @@ class Artist
     @@all
   end
 
-  def new_song(name)
+  def new_song(name, genre)
     # new song with this artist as self
+    Song.new(name, self, genre)
   end
 
   def songs
     # array of all of songs with this artist
+    Songs.all.select {|s| s.artist = self}
   end
 
   def genres
+    songs.map {|s| s.genre }
     # collects all the different genres of the artist from their songs list
   end
 end
